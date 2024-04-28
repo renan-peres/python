dfe47903e8fc2399c3bfe2e343234ff6c8a93731
Renan Peres <renanmogo2@protonmail.com>
2024-04-28 10:29:06 -0400

    Polars Folder Upload

---
+++ polars/02-polars-materials-course/pip-deploy.sh
@@ -0,0 +1,7 @@
+echo "In this script we create a virtual environment for the Polars course and install the required packages"
+# Create a virtual environment
+echo "Create virtual environment"
+python -m venv course_env
+# Activate the virtual environment
+echo "Activate the virtual environment, upgrade pip and install the packages into the virtual environment"
+source course_env/bin/activate && pip install --upgrade pip && pip install -r requirements.txt
